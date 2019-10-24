9b5bd5d1a6b19cc94ff8d3fd59e99361a2cb8d9c
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2019-10-24 10:52:39 +0000

    init

---
+++ src/io/turtabl/Main.java
@@ -0,0 +1,12 @@
+package io.turtabl;
+
+public class Main {
+
+    //todo: work on the main method
+    public static void main(String[] args) {
+        System.out.println("Hello, turntabl!");
+    }
+}
+
+
+
